7e045a38b7f7c9e56381dc2467d24122feafe1f3
Tsering Lama <tseringlama@pursuit.org>
2020-02-06 12:52:19 -0500

    created a APIClient page with url session

--- APIClient/NYTimesAPI.swift
+++ APIClient/NYTimesAPI.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import NetworkHelper
+
+struct NYTimesAPI {
+    static func getTopStories(section: String, completionHandler: @escaping(Result<[Article], AppError>) -> ()) {
+        let endpointURL = ""
+        guard let url = URL(string: endpointURL) else {
+            completionHandler(.failure(.badURL(endpointURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completionHandler(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let newsData = try JSONDecoder().decode(TopStories.self, from: data)
+                    completionHandler(.success(newsData.results))
+                } catch {
+                    completionHandler(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
