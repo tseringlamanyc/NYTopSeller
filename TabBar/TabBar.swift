6b385b77b63a90bb7a72cc889064cd8ae934ef56
Tsering Lama <tseringlama@pursuit.org>
2020-02-06 10:49:01 -0500

    Initial Commit

---
+++ TabBar/TabBar.swift
@@ -0,0 +1,17 @@
+//
+//  TabBar.swift
+//  NYTopSeller
+//
+//  Created by Tsering Lama on 2/6/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+import UIKit
+
+class TabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
